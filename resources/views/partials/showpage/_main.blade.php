481f8f601cf9c4bda6a1b39242e245ab3eb75e49
maclemire <pierrelemire@hotmail.fr>
2022-09-19 16:27:03 +0200

    debut-offrepage

--- resources/views/partials/showpage/_main.blade.php
+++ resources/views/partials/showpage/_main.blade.php
@@ -1,6 +1,13 @@
-<div class="grid grid-cols-2 gap-4 px-36">
+<div class="grid grid-cols-2 gap-12 px-60 pt-24">
   <div class="">
-    <p class="text-4xl font-bold pb-6 text-center">Sélectionnez le XC90 Recharge</p>
+    <p class="text-4xl font-bold pb-24">Sélectionnez le XC90 Recharge</p>
     <img src="img/show1.jpg" alt="xc90 recharge">
+  </div>
+  <div class="">
+    <p class="text-sm text-gray-400 font-bold pb-12 text-end pl-72">Découvrez nos offres et nos services imaginés pour vous aider à construire votre projet</p>
+    <p class="text-4xl font-bold pb-6 pr-12">Personnalisez votre conduite. Configurez et comparez pour choisir votre Volvo idéale</p>
+    <p class="text-sm font-bold pb-6 ">A partir de 90 800€</p>
+    <p class="text-sm  pb-6 "></p>
+    <a href=""></a>
   </div>
 </div>
